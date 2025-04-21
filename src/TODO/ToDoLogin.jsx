dcaee5c929e5e598866b2c90ab66f18f8813a30c
Biswajit Mahankuda <biswajitmahankuda9@gmail.com>
2025-04-21 23:38:32 +0530

    update cookies error

--- src/TODO/ToDoLogin.jsx
+++ src/TODO/ToDoLogin.jsx
@@ -6,7 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 export function ToDoLogin() {
     let navigate=useNavigate();
-    const [setCookies] = useCookies(['userid']);
+    const [,setCookies] = useCookies(['userid']);
     const formik = useFormik({
         initialValues: {
             userid: '',
